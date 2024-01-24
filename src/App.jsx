889a7259342703f377ebe135ead0644dc424bfad
Jamie Chan <jamiechan@Jamies-MacBook-Pro.local>
2024-01-24 10:48:48 -0800

    feat: configured the react router for different colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div id="container">
-        <h1>Hello React Router!</h1>
         <div id="navbar">{/* navigation here */}</div>
-        <div id="main-section">{/* routes here */}</div>
+        <div id="main-section">
+          <div id="main-section">
+            <Routes>
+              <Route path="/blue" element={<h1>Blue</h1>} />
+              <Route path="/red" element={<h1>Red</h1>} />
+            </Routes>
+          </div>
+        </div>
       </div>
     </>
   );
